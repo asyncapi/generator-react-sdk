c6654cf3f61f80c1db4ecf7e9a68b6370c0c4c4a
jonaslagoni <jonaslagoni@localhost.localdomain>
2020-11-26 15:55:20 +0100

    Removed temp files and reintegrated component test

--- src/components/__tests__/Text.spec.tsx
+++ src/components/__tests__/Text.spec.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { IndentationTypes } from '../../utils';
 import { Text, render } from "../..";
 
 describe('<Text />', () => {
-  test('should render text as is', () => {
-    const content = render(<Text>Test</Text>);
-    expect(content).toEqual("Test\n");
+  test('Should always render text as is', () => {
+    const defaultProps = {};
+    const wrapper = render(<Text {...defaultProps}>Test</Text>);
+    expect(wrapper).toMatchSnapshot('text_should_render');
+  });
+  test('Should always render indentation with spaces', () => {
+    const defaultProps = {
+      indent: 4,
+      type: IndentationTypes.SPACES
+    };
+    const wrapper = render(<Text {...defaultProps}>Test</Text>);
+    expect(wrapper).toMatchSnapshot('text_should_render_space_indentation');
+  });
+  test('Should always render indentation with tabs', () => {
+    const defaultProps = {
+      indent: 4,
+      type: IndentationTypes.TABS
+    };
+    const wrapper = render(<Text {...defaultProps}>Test</Text>);
+    expect(wrapper).toMatchSnapshot('text_should_render_tab_indentation');
   });
 });
--- temp/index.js
+++
@@ -1,4 +0,0 @@
-export {default as File} from './template/components/File';
-export {default as Indent} from './template/components/Indent';
-export {default as Text} from './template/components/Text';
-export { IndentationTypes } from './template/components/enums/IndentationTypes';
