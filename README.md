cee3f88d9c13bcbc71668b3104e162c73e6235f9
jonaslagoni <jonaslagoni@localhost.localdomain>
2020-12-01 13:18:46 +0100

    Added some docs and re-organized the transpiler

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # generator-react-sdk
 React SDK
+
+
+
+## The Transpile Process
+
+All the files within the template folder gets transpiled using [rollup](https://www.npmjs.com/package/rollup). Rollup helps bundling all dependencies and transpile them into CommonJS modules. This is required because this library will be used through NodeJS which does not understand these new modules natively and we do not want to limit the develop in which syntax they prefer.
+
+## <a name="rendering_process"></a>The Rendering Process
+
+
+
+### The debug flag
+When rendering you have the option of passing a `debug` flag which does
+
+* Does not remove the transpiled files after the rendering process is done.
+
+
--- src/renderer/template.ts
+++ src/renderer/template.ts
@@ -2,12 +2,8 @@ import React from "react";
 import { render } from "./renderer";
 import Path from 'path';
 import Fs from 'fs';
-import {transform} from '@babel/core';
-const ROOT_DIR = Path.resolve(__dirname, '../..');
-import {rollup} from 'rollup';
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
+import { transpileFiles } from "./transpiler";
+
 /**
  * Imports a given file and return the imported component
  * 
@@ -19,75 +15,8 @@ function importComponent(filepath:string): Promise<React.ReactElement> {
   })
 }
 
-async function transpileFilesRollup(filepath: string, outputDir: string){
-  const parsedFile = Path.parse(filepath)
-  const dir = parsedFile.dir
-  const files = Fs.readdirSync(dir);
-  if (!Fs.existsSync(outputDir)){
-    Fs.mkdirSync(outputDir);
-  }
-  var filePaths = files.map((value => {
-    return Path.resolve(dir, value)
-  }));
-  try{
-    const bundle = await rollup({
-      input: filePaths,
-      plugins: [
-        babel({
-          cwd: ROOT_DIR,
-          presets: [
-            "@babel/preset-env",
-            "@babel/preset-react"
-          ],
-        }),
-        commonjs(),
-      ]
-    })
 
-    await bundle.write({
-      format: "commonjs",
-      sourcemap: true,
-      dir: outputDir
-    })
-
-  }catch(e) {
-    console.log(e);
-  }
-
-}
-
-function transpileFilesBabel(filepath: string, outputDir: string){
-  const parsedFile = Path.parse(filepath)
-  const dir = parsedFile.dir
-  const files = Fs.readdirSync(dir);
-  if (!Fs.existsSync(outputDir)){
-    Fs.mkdirSync(outputDir);
-  }
-
-  files.forEach(file => {
-    const filePathString = Path.resolve(dir, file)
-    const filePath = Path.parse(filePathString);
-    const filecontent = Fs.readFileSync(filePathString);
-    const filecontentString = filecontent.toString();
-    var babelTransformed = transform(filecontentString, {
-      cwd: ROOT_DIR,
-      sourceMaps: "inline",
-      sourceRoot: dir,
-      plugins: [
-        "source-map-support"
-      ],
-      presets: [
-        "@babel/preset-env",
-        "@babel/preset-react"
-      ],
-      sourceFileName: filePath.base
-    })
-    const outputFile = Path.resolve(outputDir, filePath.base);
-    const outputContent = String(babelTransformed?.code);
-    Fs.writeFileSync(outputFile, outputContent)
-  })
-}
-
+    
 /**
  * render a file with react. This function automatically transforms jsx to js before importing the component.
  * 
@@ -98,7 +27,7 @@ export async function renderTemplate(filepath: string, context: any, debug: bool
   const dir = parsedFile.dir
   const outputDir = Path.resolve(dir, '../__transpiled');
   //compile the template dir first
-  await transpileFilesRollup(filepath, outputDir);
+  await transpileFiles(filepath, outputDir, {recursive: true});
 
   
   const inputFile = Path.resolve(outputDir, parsedFile.base);
